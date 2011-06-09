20ba46420485cde214a3518cdf1238771f25228a
Vilson Vieira <vilson@void.cc>
2011-06-09 18:47:08 -0300

    started LI7E API

---
+++ src/li7e.js
@@ -0,0 +1,37 @@
+//     LI7E - A framework for creative code on the Web
+
+(function () {
+
+// Starting...
+// -----------
+
+// This is the only var globally visible outside. Every class
+// is attached to this.
+var LI7E;
+
+// Auxiliary Functions
+// -------------------
+
+// Show debug messages to appropriated output
+LI7E.debug = function (msg) {
+    console.log("LI7E DEBUG", msg);
+};
+
+// Graphics
+// --------
+
+// Audio
+// -----
+
+// Physical Engine
+// ---------------
+
+// DSL
+// ---
+
+// Here starts the LI7E DSL
+
+// Editor
+// ------
+
+})();
